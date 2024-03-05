ef77f5016af2b332590dee559a1b39e02f8d41ad
Michael Collins <michael-collins@users.noreply.github.com>
2024-03-05 11:04:17 -0500

    added ui

---
+++ .vscode/settings.js
@@ -0,0 +1,14 @@
+{
+    "files.associations": {
+        "*.css": "tailwindcss"
+    },
+    "editor.quickSuggestions": {
+        "strings": true
+    },
+    "tailwindCSS.experimental.classRegex": [
+        ["ui:\\s*{([^)]*)\\s*}", "[\"'`]([^\"'`]*).*?[\"'`]"],
+        ["/\\*ui\\*/\\s*{([^;]*)}", ":\\s*[\"'`]([^\"'`]*).*?[\"'`]"]
+      ]
+  }
+  
+  
