d5a33f6adeb7c4bcfe6a2fd0c1704a194407c1ef
Michael Collins <michael-collins@users.noreply.github.com>
2024-03-17 15:46:45 -0400

    quick fix for keyboard nav for menus

--- components/navBar.vue
+++ components/navBar.vue
@@ -6,20 +6,20 @@
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
         </div>
         <ul class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-          <li><nuxt-link exact class="nav-link" role="button" tabindex="0" to="/" aria-label="About page">About</nuxt-link></li>
-          <li><nuxt-link class="nav-link" role="button" tabindex="-1" to="/lessons" aria-label="Lessons page">Lessons</nuxt-link></li>
-          <li><nuxt-link class="nav-link" role="button" tabindex="-1" to="/lectures" aria-label="Lectures page">Lectures</nuxt-link></li>
-          <li><nuxt-link class="nav-link" role="button" tabindex="-1" to="/pathways/" aria-label="Pathways page">Pathways</nuxt-link></li>
-          <li><nuxt-link class="nav-link" role="button" tabindex="-1" to="/exercises/" aria-label="Exercises page">Exercises</nuxt-link></li>
+          <li><nuxt-link class="nav-link" role="button" tabindex="1" to="/" ><button aria-label="About page">About</button></nuxt-link></li>
+        <li><nuxt-link class="nav-link" role="button" tabindex="1" to="/lessons"><button aria-label="About page">Lessons</button></nuxt-link></li>
+        <li><nuxt-link class="nav-link" role="button" tabindex="1" to="/lectures"><button aria-label="Lectures page">Lectures</button></nuxt-link></li>
+        <li><nuxt-link class="nav-link" role="button" tabindex="1" to="/pathways/"><button aria-label="Pathways page">Pathways</button></nuxt-link></li>
+        <li><nuxt-link class="nav-link" role="button" tabindex="1" to="/exercises/"><button aria-label="Exercises page">Exercises</button></nuxt-link></li>
         </ul>
       </nav>
       <nav class="flex-1 hidden md:flex">
       <ul class="menu menu-horizontal px-1">
-        <li><nuxt-link exact class="nav-link" role="button" tabindex="-1" to="/" aria-label="About page">About</nuxt-link></li>
-        <li><nuxt-link class="nav-link" role="button" tabindex="-1" to="/lessons" aria-label="Lessons page">Lessons</nuxt-link></li>
-        <li><nuxt-link class="nav-link" role="button" tabindex="-1" to="/lectures" aria-label="Lectures page">Lectures</nuxt-link></li>
-        <li><nuxt-link class="nav-link" role="button" tabindex="-1" to="/pathways/" aria-label="Pathways page">Pathways</nuxt-link></li>
-        <li><nuxt-link class="nav-link" role="button" tabindex="-1" to="/exercises/" aria-label="Exercises page">Exercises</nuxt-link></li>
+        <li><nuxt-link class="nav-link" role="button" tabindex="1" to="/" ><button aria-label="About page">About</button></nuxt-link></li>
+        <li><nuxt-link class="nav-link" role="button" tabindex="1" to="/lessons"><button aria-label="About page">Lessons</button></nuxt-link></li>
+        <li><nuxt-link class="nav-link" role="button" tabindex="1" to="/lectures"><button aria-label="Lectures page">Lectures</button></nuxt-link></li>
+        <li><nuxt-link class="nav-link" role="button" tabindex="1" to="/pathways/"><button aria-label="Pathways page">Pathways</button></nuxt-link></li>
+        <li><nuxt-link class="nav-link" role="button" tabindex="1" to="/exercises/"><button aria-label="Exercises page">Exercises</button></nuxt-link></li>
       </ul>
     </nav>
     </div>
