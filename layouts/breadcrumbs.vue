9a2a7ccb7ed1339d0886b61b0e9eac02b5d87e92
Michael Collins <michael-collins@users.noreply.github.com>
2024-09-01 10:26:16 -0400

    removed sticky hamburger

--- layouts/breadcrumbs.vue
+++ layouts/breadcrumbs.vue
@@ -7,7 +7,7 @@
       <div class="drawer-content">
         <a href="#main-content" class="skip-link">Skip to main content</a>
 
-        <label for="side-navigation-drawer" class="btn btn-ghost drawer-button lg:hidden left m-1 sticky top-0">
+        <label for="side-navigation-drawer" class="btn btn-ghost drawer-button lg:hidden left m-1">
           <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg> -->
           <Icon name="charm:menu-hamburger" class="text-xl" />
         </label>
