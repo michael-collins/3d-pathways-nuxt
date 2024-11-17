26991da1337b0610b04a71379c9f62f81ebd0b70
Michael Collins <michael-collins@users.noreply.github.com>
2024-11-17 14:51:23 -0500

    cleaned up key

--- services/AirtableService.js
+++ services/AirtableService.js
@@ -3,14 +3,19 @@ import Airtable from 'airtable'
 
 export async function getAirtableRecords(tableName, apiKey) {
   if (!apiKey) {
-    throw new Error('Airtable API key is required')
+    throw new Error('Airtable API key is required');
   }
 
-  const base = new Airtable({ apiKey }).base('appErImxpeRDom8je')
-  const records = await base(tableName).select({ view: 'Grid view' }).all()
-  
+  const baseId = process.env.AIRTABLE_BASE_ID;
+  if (!baseId) {
+    throw new Error('Airtable Base ID is required');
+  }
+
+  const base = new Airtable({ apiKey }).base(baseId);
+  const records = await base(tableName).select({ view: 'Grid view' }).all();
+
   return records.map(record => ({
     id: record.id,
     fields: record.fields
-  }))
+  }));
 }
